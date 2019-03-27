49344142bfc0168ea1fbfcb1d7ac059bb8855e7f
Julia <jtstamand@brandeis.edu>
2019-03-27 14:44:00 -0400

    testing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -71,6 +71,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:description, :integer)
+      params.require(:item).permit(:description, :int)
     end
 end
